29731dffe1e58b940748dc4ec6a27a7cbec387a8
Adrian Trifan <vadrian.trifan@gmail.com>
2017-06-28 21:31:45 +0300

    First commit

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var nosql = require('./nosql_controller.js');
+
+
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+		nosql.insert();
+		nosql.read_last();
+    res.render('index', {
+        title: 'Wireless Car Control'
+    });
+
+});
+router.get('/carinfo', function(req, res, next) {
+    var nosql = require('./nosql_controller.js');
+    res.send(nosql.last);
+});
+
+
+
+
+module.exports = router;
